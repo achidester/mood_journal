7cfe25fa4c686b2dc87b49b3293bd6e10115f7f6
achidester <chidesta@oregonstate.edu>
2022-07-11 21:57:18 -0700

    Added something to handle if the user gives an answer that isnt within the hash. Note: I want to add a microservice that will save the users mood description to the hash and save it.

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+#possibly break this into smaller hashes that will help the computer respond in certain ways based on mood.
+
 mood_hash = {
     "sad": "\nSurely tomorrow will be better",
     "happy": "\nThat's great news!",
@@ -9,6 +11,17 @@ print("How are you feeling today?", f"\n")
 
 user_mood = input()
 
-print(mood_hash[user_mood], f"\n")
+#responding to the mood
+#else: ask user to describe it (add mood to the hash table) !Microservice needed to dynamically add things to hash!
+
+if user_mood in mood_hash:
+    print(mood_hash[user_mood], f"\n")
+else:
+    print("I haven't heard that one, what does it feel like?")
+    mood_value = input()
+    mood_hash[user_mood] = mood_value
+
+
+
 
 print("Here are some journal writing prompts for today...", f"\n")
