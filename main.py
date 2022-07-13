7a790864631abee0f0e507b8b1a4986f750f5f68
achidester <chidesta@oregonstate.edu>
2022-07-12 21:15:56 -0700

    Added text input and set the page size to be constant.

--- main.py
+++ main.py
@@ -1,15 +1,37 @@
 # Mood Journal App
 
 
-import tkinter as tk
+from cProfile import label
+from tkinter import *
+
 
 #I watched this video for all tkinter (https://www.youtube.com/watch?v=YXPyB4XeYLA)
-window = tk.Tk()
-canvas = tk.Canvas(window, width=800, height=500)
-canvas.grid(columnspan=5)
-img = tk.PhotoImage(file="logo.png")
-img.subsample(20,20)
-canvas.create_image(400,100,image=img)
+
+
+window = Tk()
+window.configure(bg='white')
+window.geometry('630x600')
+window.maxsize(630,600)
+window.minsize(630,600)
+
+# Top logo
+canvas = Canvas(window, width=630, height=200, bg="white")
+img = PhotoImage(file="logo.png")
+canvas.create_image(330,100,image=img)
+canvas.pack(side=TOP)
+
+
+# intro question
+introduction = Label(window, bg="white", text ='How are you feeling today?')
+introduction.pack()
+
+# user_mood entry
+user_mood = Entry(window, width=20)
+user_mood.pack()
+
+
+window.mainloop()
+
 
 
 #possibly break this into smaller hashes that will help the computer respond in certain ways based on mood.
@@ -20,19 +42,19 @@ mood_hash = {
 }
 
 
-print("How are you feeling today?", f"\n")
+# print("How are you feeling today?", f"\n")
 
-user_mood = input()
+# user_mood = input()
 
-#responding to the mood
-#else: ask user to describe it (add mood to the hash table) !Microservice needed to dynamically add things to hash!
+# #responding to the mood
+# #else: ask user to describe it (add mood to the hash table) !Microservice needed to dynamically add things to hash!
 
-if user_mood in mood_hash:
-    print(mood_hash[user_mood], f"\n")
-else:
-    print("I haven't heard that one, what does it feel like?")
-    mood_value = input()
-    mood_hash[user_mood] = mood_value
+# if user_mood in mood_hash:
+#     print(mood_hash[user_mood], f"\n")
+# else:
+#     print("I haven't heard that one, what does it feel like?")
+#     mood_value = input()
+#     mood_hash[user_mood] = mood_value
 
 
-print("Here are some journal writing prompts for today...", f"\n")
+# print("Here are some journal writing prompts for today...", f"\n")
